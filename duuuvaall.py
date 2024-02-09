826796159cffd98a96e0245965a8995d151fcc87
William L. Thomson Jr. <wlt@o-sinc.com>
2024-02-09 17:18:37 -0500

    duuuvaall.py: Included class

---
+++ duuuvaall.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+duuuvaall class represents a chant said in duuuvaall
+"""
+class duuuvaall:
+
+    def __init__(self):
+        """
+        duuuvaall class represents a chant said in duuuvaall
+        """
+        print("duuuvaall")
